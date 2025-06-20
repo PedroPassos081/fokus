a8645210964589bb02c77c810ad7767fbf648e73
Pedro Passos <pedro.passos081@gmail.com>
2025-06-19 22:38:16 -0300

    Iniciando o styleSheet

--- app/index.jsx
+++ app/index.jsx
@@ -1,15 +1,21 @@
-import { Text, View } from "react-native";
+import { StyleSheet, Text, View } from "react-native";
 
 export default function Index() {
   return (
-    <View
-      style={{
-        flex: 1,
-        justifyContent: "center",
-        alignItems: "center",
-      }}
-    >
-      <Text>Teste</Text>
+    <View style={styles.container}>
+      <Text style={styles.text}>Teste</Text>
     </View>
   );
 }
+
+const styles = StyleSheet.create({
+  container: {
+    flex: 1,
+    justifyContent: "center",
+    alignItems: "center",
+    backgroundColor: "#021123",
+  },
+  text: {
+    color: "#fff",
+  },
+});
