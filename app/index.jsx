44e83704ecbecf077a06ae08c330d52d474a57db
Pedro Passos <pedro.passos081@gmail.com>
2025-06-25 19:43:42 -0300

    Iniciando a tela inicial do projeto, com botao e o timer

--- app/index.jsx
+++ app/index.jsx
@@ -1,9 +1,21 @@
-import { StyleSheet, Text, View } from "react-native";
+import { Image, Pressable, StyleSheet, Text, View } from "react-native";
 
 export default function Index() {
   return (
     <View style={styles.container}>
-      <Text style={styles.text}>Teste</Text>
+      <Image source={require("../assets/images/foco.png")} />
+
+      <View style={styles.actions}>
+        <Text style={styles.timer}>25:00</Text>
+        <Pressable style={styles.button}>
+          <Text style={styles.buttonText}>Começar</Text>
+        </Pressable>
+      </View>
+      <View style={styles.footer}>
+        <Text style={styles.footerText}>
+          Projeto fictício e sem fins comerciais.
+        </Text>
+      </View>
     </View>
   );
 }
@@ -14,8 +26,39 @@ const styles = StyleSheet.create({
     justifyContent: "center",
     alignItems: "center",
     backgroundColor: "#021123",
+    gap: 40,
   },
-  text: {
+  actions: {
+    paddingVertical: 24,
+    paddingHorizontal: 24,
+    backgroundColor: "#14448080",
+    width: "80%",
+    borderRadius: 32,
+    borderWidth: 2,
+    borderColor: "#144480",
+    gap: 32,
+  },
+  timer: {
+    fontSize: 54,
     color: "#fff",
+    fontWeight: "bold",
+    textAlign: "center",
+  },
+  button: {
+    backgroundColor: "#b872ff",
+    borderRadius: 32,
+    padding: 10,
+  },
+  buttonText: {
+    textAlign: "center",
+    color: "#021123",
+    fontSize: 18,
+    fontWeight: "bold",
+  },
+  footer: { width: "80%" },
+  footerText: {
+    textAlign: "center",
+    color: "#98A0AB",
+    fontSize: 12.5,
   },
 });
