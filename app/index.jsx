6288121f16cc5f8453980e96b42a3477851e457b
Pedro Passos <pedro.passos081@gmail.com>
2025-07-16 21:20:20 -0300

    Finalizando o projeto colocando o timer pra funcionar e os icones no botao de começar e pausar

--- app/index.jsx
+++ app/index.jsx
@@ -1,25 +1,26 @@
-import { useState } from "react";
+import { useRef, useState } from "react";
 import { Image, StyleSheet, Text, View } from "react-native";
 import { ActionButton } from "../components/ActionButton";
 import { FokusButton } from "../components/FokusButton";
+import { IconPause, IconPlay } from "../components/Icons";
 import { Timer } from "../components/Timer";
 
 const pomodoro = [
   {
     id: "focus",
-    initialValue: 25,
+    initialValue: 25 * 60,
     Image: require("../assets/images/foco.png"),
     display: "foco",
   },
   {
     id: "short",
-    initialValue: 5,
+    initialValue: 5 * 60,
     Image: require("../assets/images/short.png"),
     display: "pausa curta",
   },
   {
     id: "long",
-    initialValue: 15,
+    initialValue: 15 * 60,
     Image: require("../assets/images/long.png"),
     display: "pausa longa",
   },
@@ -27,7 +28,44 @@ const pomodoro = [
 
 export default function Index() {
   const [timerType, setTimerType] = useState(pomodoro[0]);
+  const [seconds, setSeconds] = useState(pomodoro[0].initialValue);
+  const [timerRunning, setTimerRunning] = useState(false);
 
+  const timerRef = useRef(null);
+
+  const clear = () => {
+    if (timerRef.current != null) {
+      clearInterval(timerRef.current);
+      timerRef.current = null;
+      setTimerRunning(false);
+    }
+  };
+
+  const toggleTimerType = (newTimerType) => {
+    setTimerType(newTimerType);
+    setSeconds(newTimerType.initialValue);
+    clear();
+  };
+
+  const toggleTimer = () => {
+    if (timerRef.current) {
+      clear();
+      return;
+    }
+
+    setTimerRunning(true);
+
+    const id = setInterval(() => {
+      setSeconds((oldState) => {
+        if (oldState === 0) {
+          clear();
+          return timerType.initialValue;
+        }
+        return oldState - 1;
+      });
+    }, 1000);
+    timerRef.current = id;
+  };
   return (
     <View style={styles.container}>
       <Image source={timerType.Image} />
@@ -38,13 +76,17 @@ export default function Index() {
             <ActionButton
               key={p.id}
               active={timerType.id === p.id}
-              onPress={() => setTimerType(p)}
+              onPress={() => toggleTimerType(p)}
               display={p.display}
             />
           ))}
         </View>
-        <Timer totalSeconds={timerType.initialValue * 60} />
-        <FokusButton />
+        <Timer totalSeconds={seconds} />
+        <FokusButton
+          title={timerRunning ? "Pausar" : "Começar"}
+          icon={timerRunning ? <IconPause /> : <IconPlay />}
+          onpress={toggleTimer}
+        />
       </View>
       <View style={styles.footer}>
         <Text style={styles.footerText}>
--- components/FokusButton/index.jsx
+++ components/FokusButton/index.jsx
@@ -1,23 +1,27 @@
 import { Pressable, StyleSheet, Text } from "react-native";
 
-export const FokusButton = () => {
+export const FokusButton = ({ onpress, title, icon }) => {
   return (
-    <Pressable style={styles.button}>
-      <Text style={styles.buttonText}>Começar</Text>
+    <Pressable style={styles.button} onPress={onpress}>
+      {icon}
+      <Text style={styles.buttonText}>{title}</Text>
     </Pressable>
   );
 };
 
 const styles = StyleSheet.create({
   button: {
-    backgroundColor: "#b872ff",
+    backgroundColor: "#B872FF",
     borderRadius: 32,
-    padding: 10,
+    padding: 8,
+    flexDirection: "row",
+    gap: 12,
+    alignItems: "center",
+    justifyContent: "center",
   },
   buttonText: {
     textAlign: "center",
     color: "#021123",
     fontSize: 18,
-    fontWeight: "bold",
   },
 });
